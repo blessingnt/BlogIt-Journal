2cda1065a04dec07fbbccd9569daded8243b1075
blessing <blessingntekume@gmail.com>
2022-02-01 01:33:00 -0800

    Set DEBUG=False for Heroku.

--- blog/settings.py
+++ blog/settings.py
@@ -152,8 +152,11 @@ if os.getcwd() == '/app':
     # Honor the 'X-Forwarded-Proto' header for request.is_secure().
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-    # Allow all host headers.
-    ALLOWED_HOSTS = ['*']
+
+    # Allow only Heroku to host the project.
+    ALLOWED_HOSTS = ['blogit-journal.herokuapp.com']
+
+    DEBUG = False
 
     # Static asset configuration
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
